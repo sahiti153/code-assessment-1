05c1d85e31e8839078bb0d677b5e6267334e0313
Sahiti Srikakolapu <149192813+sahiti153@users.noreply.github.com>
2025-10-30 15:33:19 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://user:password@localhost/taskdb'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = 'smtp.example.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
